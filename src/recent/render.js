68fd4213e34c903177acff05e156700c0f7c4f68
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-10-03 13:19:55 +0530

    Added recent

--- src/recent/render.js
+++ src/recent/render.js
@@ -1,9 +1,27 @@
+pname = "Test"
+
 $(document).ready(function() {
+    for(let i=0; i<5; i++)
+    {
     $('#outerdiv').append(
-        $('<div>').prop({
-            id: 'container',
-            innerHTML: "<h1>Helllo<h1/>",
-            className: 'container'
-        })
+      $('<div>').prop({
+        id: 'innerdiv',
+        innerHTML: 
+                `<a>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div class="btn" id="load">${pname} ${i}</div>
+                </a><br>
+                `,
+        className: 'container'
+      })
     );
-});
+    }
+  });
+
+$('#home').click(function() {
+    window.location.replace("../");
+})
+  
